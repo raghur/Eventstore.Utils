a4e3accf018ec1270e5dfb205697829b6833cd3e
Raghu Rajagopalan <raghu.rajagopalan@gmail.com>
2016-02-14 14:48:34 +0530

    Script: run Gitlink and Nuget pack

---
+++ build-package.bat
@@ -0,0 +1,3 @@
+Gitlink . -c Debug -u https://github.com/raghur/Eventstore.Utils
+cd Eventstore.Utils
+nuget pack Eventstore.Utils.csproj -verbosity detailed -includereferencedprojects -Prop Platform=AnyCPU
